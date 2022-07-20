81957fc0372881ebffc77dc9c03685430a6b5afd
Louie - work <codelouie94@gmail.com>
2022-07-19 23:13:37 -0700

    hello world

---
+++ hello_world.py
@@ -0,0 +1,20 @@
+# 1. TASK: print "Hello World"
+print('Hello World')
+# 2. print "Hello Noelle!" with the name in a variable
+name1 = "Noelle"
+my_name = 'Louie'
+print('Hello', my_name)	# with a comma
+print('Hello ' + my_name)	# with a +
+# 3. print "Hello 42!" with the number in a variable
+name2 = 42
+num = 3
+print('Hello', num)	# with a comma
+print('Hello ' + str(num))	# with a +	-- this one should give us an error!
+# 4. print "I love to eat sushi and pizza." with the foods in variables
+fave_food1 = "sushi"
+fave_food2 = "pizza"
+food1 = "cereal"
+food2 = "chicken afritada"
+print('I love to eat {} and {}.'.format (food1, food2)) # with .format()
+print(f'I love to eat %s and %s.' % (food1, food2)) # with an f string
+
