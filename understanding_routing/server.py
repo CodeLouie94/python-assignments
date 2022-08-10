9f85f635802bf1663338e15b6ec1eb9434065e03
Louie94 <91927760+CodeLouie94@users.noreply.github.com>
2022-08-09 23:31:54 -0700

    Understanding routing

---
+++ understanding_routing/server.py
@@ -0,0 +1,22 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+@app.route('/dojo')
+def dojo():
+    return 'Dojo!'
+
+@app.route('/say/<string:name>')
+def say(name):
+    return f"Hi {name}!"
+
+@app.route('/repeat/<int:num>/<greeting>')
+def repeat(greeting, num):
+    return f"{greeting * num} "
+
+if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
+    app.run(debug=True)    # Run the app in debug mode.
+
